3a853c2906c56647e7cbcca656676169680934be
Peter Hickman <peterhickman386@googlemail.com>
2015-05-01 14:40:01 +0100

    strings are not allways encoded like that

--- measure_sql.rb
+++ measure_sql.rb
@@ -17,7 +17,7 @@ def clean_select(line)
     gsub(/.\[.m/,'').       # Remove the ansi escape codes that the log adds
     gsub(/.\[..m/,'').      # Remove the ansi escape codes that the log adds
     gsub(/[0-9]+/,'x').     # Squish a run of digits
-    gsub(/E'[^']+'/, "'s'") # Squish a run of characters
+    gsub(/'[^']+'/, "'s'")  # Squish a run of characters
 
   ##
   # Compresses (x,x,x,x...x) down to (x)
